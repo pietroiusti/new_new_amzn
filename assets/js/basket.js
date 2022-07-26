ba0e7b3d32523108d3e68afa0664710ad0e6b9a1
glptrst <giulio.pietroiusti@gmail.com>
2022-07-26 23:28:35 +0200

    Remove old basket.

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,21 +1,3 @@
-// class Basket extends HTMLElement {
-//   constructor() {
-//     super();
-//   }
-//   connectedCallback() {
-//     this.innerHTML = `
-//         <div id="totalNumberDiv">Total number of items: 0</div>
-//         <div id="totalPriceDiv">Total Price: 0.00 $</div>
-//     `;
-//   }
-
-//   attributeChangedCallback() {
-//     console.log('hello world');
-//   }
-// }
-// customElements.define('basket-el', Basket);
-
-
 class Basket extends HTMLElement {
   constructor() {
     super();
