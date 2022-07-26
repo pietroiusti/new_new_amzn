d4e8a07013898aa3e50fdfa6ded4677efb7354b2
Giulio <giulio@prenomics.com>
2022-07-26 16:51:44 +0200

    Turn basket into component.

--- assets/js/myamazon.js
+++ assets/js/myamazon.js
@@ -41,14 +41,32 @@ class Item extends HTMLElement {
     `;
   }
 }
+customElements.define('list-el', Item);
 
-customElements.define('list-item', Item);
+class Basket extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+        <div id="totalNumberDiv">Total number of items: 0</div>
+        <div id="totalPriceDiv">Total Price: 0.00 $</div>
+    `;
+  }
+}
+customElements.define('basket-el', Basket);
 
-function render_list_of_items2() {
+function renderBasket() {
+  let basketDiv = document.getElementById('basketDiv');
+  let basket = document.createElement('basket-el');
+  basketDiv.appendChild(basket);
+}
+
+function render_list_of_items() {
   let itemList = document.getElementById('itemList');
   for (let i = 0; i < data.length; i++) {
     let name = data[i].name;
-    let item = document.createElement('list-item');
+    let item = document.createElement('list-el');
     item.setAttribute('name', name);
     item.setAttribute('price', data[i].price);
     item.setAttribute('imgSrc', `./assets/img/${name}.jpg`);
@@ -83,77 +101,6 @@ function removeItem(event) {
   updateBasket()
 }
 
-function render_list_of_items() {
-  let itemList = document.getElementById('itemList');
-  for (let i = 0; i < data.length; i++) {
-    let item = document.createElement('div');
-    item.classList.add('item');
-    let name = data[i].name;
-    item.id = name;
-
-    let nameDiv = document.createElement('div');
-    nameDiv.classList.add('name');
-    let nameText = document.createTextNode(capitalizeFirstLetter(name));
-    nameDiv.appendChild(nameText);
-    item.appendChild(nameDiv);
-
-    let priceDiv = document.createElement('div');
-    priceDiv.classList.add('price');
-    let priceText = document.createTextNode(`${data[i].price} $`);
-    priceDiv.appendChild(priceText);
-    item.appendChild(priceDiv);
-
-    let imgDiv = document.createElement('div');
-    priceDiv.classList.add('itemImg');
-    let img = document.createElement('img');
-    img.src = `./assets/img/${data[i].name}.jpg`;
-    imgDiv.appendChild(img);
-    item.appendChild(imgDiv);
-
-    let qttyDiv = document.createElement('div');
-    qttyDiv.classList.add('qtty');
-    let qttyText = document.createTextNode(data[i].qtty);
-    qttyDiv.appendChild(qttyText);
-    item.appendChild(qttyDiv);
-
-    let buttonMinus = document.createElement('button');
-    buttonMinus.addEventListener('click', (e) => {
-      removeItem(e);
-    });
-    let buttonMinusText = document.createTextNode('-');
-    buttonMinus.appendChild(buttonMinusText);
-    item.appendChild(buttonMinus);
-
-    let buttonPlus = document.createElement('button');
-    buttonPlus.addEventListener('click', (e) => {
-      addItem(e);
-    });
-    let buttonPlusText = document.createTextNode('+');
-    buttonPlus.appendChild(buttonPlusText);
-    item.appendChild(buttonPlus);
-
-    itemList.appendChild(item);
-  }
-}
-
-function renderBasket() {
-  let basket = document.getElementById('basketDiv');
-
-  //total number of items
-  let totalNumberDiv = document.createElement('div');
-  totalNumberDiv.id = 'totalNumberDiv';
-  let totalNumberText = document.createTextNode('Total number of items: 0');
-  totalNumberDiv.appendChild(totalNumberText);
-  basket.appendChild(totalNumberDiv);
-
-  //total price
-  let totalPriceDiv = document.createElement('div');
-  totalPriceDiv.id = 'totalPriceDiv';
-  let totalPriceText = document.createTextNode('Total Price: 0.00 $');
-  totalPriceDiv.appendChild(totalPriceText);
-  basket.appendChild(totalPriceDiv);
-}
-
 function updateBasket() {
   let totalNumber = data.reduce((acc, a) => acc + a.qtty, 0);
   let totalPrice = data.reduce((acc, a) => acc + a.qtty*a.price, 0);
@@ -172,5 +119,5 @@ function capitalizeFirstLetter(string) {
 
 (function init() {
   renderBasket();
-  render_list_of_items2();
+  render_list_of_items();
 })();
