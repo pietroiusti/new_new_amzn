89a076c2549eee1d978013a813dac030942a4fa8
Giulio <giulio@prenomics.com>
2022-07-28 17:54:16 +0200

    Turn basket into ``stateful'' (``smart'') component.
    Just as an exercise.

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -5,31 +5,13 @@
 class Basket extends HTMLElement {
   constructor() {
     super();
-    //this.totalNumber
-    this.items = 'Total number of items: 0';
-    this.price = 'Total Price: 0.00 $';
   }
 
   connectedCallback() {
     this.innerHTML = `
-        <div id="totalNumberDiv">${this.items}</div>
-        <div id="totalPriceDiv">${this.price}</div>
+        <div id="totalNumberDiv">Total number of items: 0</div>
+        <div id="totalPriceDiv">Total Price: 0.00 $</div>
     `;
-  }
-
-  static get observedAttributes() {
-    return ['items', 'price', ];
-  }
-
-  attributeChangedCallback(name, oldVal, newVal) {
-    // update view
-    if (name === 'items') {
-      document.getElementById('totalNumberDiv').innerHTML = 'Total number of items: ' + newVal;
-    } else if (name === 'price') {
-      document.getElementById('totalPriceDiv').innerHTML =  'Total Price: ' + newVal + '$';
-    } else {
-      console.log('name is ' + name + '??');
-    }
   }
 }
 
@@ -40,7 +22,7 @@ store.registerListener(function basketElCallback(data) {
   let totalPrice = data.reduce((acc, a) => acc + a.qtty*a.price, 0);
   totalPrice = (Math.round(totalPrice * 100) / 100).toFixed(2); // round to two decimal places
 
-  let basketEl = document.getElementsByTagName('basket-el')[0];
-  basketEl.setAttribute('items', totalNumber);
-  basketEl.setAttribute('price', totalPrice);
+  basket = document.getElementsByTagName('basket-el')[0];
+  basket.children[0].textContent = 'Total number of items: ' + totalNumber;
+  basket.children[1].textContent = 'Total Price: ' + totalPrice + '$';
 });
