baf599ac45e4f70e936b7b76523c896f367817cd
Giulio <giulio@prenomics.com>
2022-07-28 12:09:12 +0200

    Clean.

--- assets/js/myamazon.js
+++ assets/js/myamazon.js
@@ -1,5 +1,5 @@
 // #########################################################
-// This file assumes the existence of a global variable called {...} storing {...}
+// This file assumes the existence of a `store` global variable
 // #########################################################
 
 data = store.data;
@@ -7,40 +7,21 @@ data = store.data;
 let componentsArray = []; // When a component is created is added here
 
 function handleDataChange(newData) {
-  //console.log('I should handle the change in data');
-  //console.log(newData);
-
-  // loop over components in componentsArray to update the relevant component
+  // loop over components in componentsArray to update the relevant components
   for (let i = 0; i < componentsArray.length; i++) {
-    //console.log(componentsArray[i]);// + 'should be updated if needed');
     let component = componentsArray[i];
 
     if (component.tagName == 'BASKET-EL') {
-      //console.log(component.tagName);
-
       let totalNumber = newData.reduce((acc, a) => acc + a.qtty, 0);
       let totalPrice = newData.reduce((acc, a) => acc + a.qtty*a.price, 0);
       totalPrice = (Math.round(totalPrice * 100) / 100).toFixed(2); // round to two decimal places
 
-      //let basketEl = document.getElementsByTagName('basket-el')[0];
-      //console.log(basketEl);
-      //console.log("changing attributes of basket...")
-
       component.setAttribute('items', totalNumber);
       component.setAttribute('price', totalPrice);
     } else if (component.tagName == 'LIST-EL') {
-      //console.log(component.name);
       let item = newData.find((item) => item.name === component.name);
-      //console.log(item);
-      //console.log(item.qtty);
       component.setAttribute('qtty', item.qtty);
     }
-
-    // let componentsAttributes = component.getAttributeNames();
-    // console.log('component:')
-    // console.log(component)
-    // console.log('componentsAttributes:');
-    // console.log(componentsAttributes);
   }
 }
 
@@ -53,42 +34,22 @@ itemList.addEventListener('addItem', (e)=>{
   let name = parent.id;
   let item = data.find((item) => item.name === name);
 
-  //item.qtty += 1; // <======================================== this is
-                  // a change in state that is now be performed
-                  // through a call to store.change().
-  store.change({action: 'addItem', itemName: name}, handleDataChange); // NEW
-
-
-
-  // update card and basket
-  //let qtty = Number(parent.querySelector('.qtty').textContent);
-  //let customWrapper = parent.parentElement;
-  //customWrapper.setAttribute('qtty', item.qtty); // TESTING: NOW HANDLED BY STORE/HANDLEDATACHANGE?
-  // updateBasket(); // TESTING: NOW HANDLED BY STORE/HANDLEDATACHANGE?
+  store.change({action: 'addItem', itemName: name}, handleDataChange);
 });
+
 itemList.addEventListener('removeItem', (e)=>{
-  //console.log(e);
   let parent = event.target.parentElement;
   let name = parent.id;
   let item = data.find((item) => item.name === name);
   if (item.qtty > 0) {
-    //item.qtty -= 1; // <======================================== this is
-                  // a change in state that is now be performed
-                  // through a call to store.change().
-    store.change({action: 'removeItem', itemName: name}, handleDataChange); // NEW
-
-    //let qtty = Number(parent.querySelector('.qtty').textContent);
-    //let customWrapper = parent.parentElement;
-    //customWrapper.setAttribute('qtty', item.qtty); //TESTING: NOW HANDLED BY STORE/HANDLEDATACHANGE?
+    store.change({action: 'removeItem', itemName: name}, handleDataChange);
   }
-
-  //updateBasket() // TESTING: NOW HANDLED BY STORE/HANDLEDATACHANGE?
 });
 
 function renderBasket() {
   let basketDiv = document.getElementById('basketDiv');
   let basket = document.createElement('basket-el');
-  componentsArray.push(basket); // <=================================
+  componentsArray.push(basket);
   basketDiv.appendChild(basket);
 }
 
@@ -97,29 +58,13 @@ function render_list_of_items() {
   for (let i = 0; i < data.length; i++) {
     let name = data[i].name;
     let item = document.createElement('list-el');
-    componentsArray.push(item); // <=================================
+    componentsArray.push(item);
     item.setAttribute('name', name);
     item.setAttribute('price', data[i].price);
     item.setAttribute('imgSrc', `./assets/img/${name}.jpg`);
     item.setAttribute('qtty', data[i].qtty);
     itemList.appendChild(item);
   }
-}
-
-function updateBasket() {
-  let totalNumber = data.reduce((acc, a) => acc + a.qtty, 0);
-  let totalPrice = data.reduce((acc, a) => acc + a.qtty*a.price, 0);
-  totalPrice = (Math.round(totalPrice * 100) / 100).toFixed(2); // round to two decimal places
-
-  //console.log(totalNumber);
-  //console.log(totalPrice);
-
-  let basketEl = document.getElementsByTagName('basket-el')[0];
-  //console.log(basketEl);
-  //console.log("changing attributes of basket...")
-
-  basketEl.setAttribute('items', totalNumber);
-  basketEl.setAttribute('price', totalPrice);
 }
 
 function capitalizeFirstLetter(string) {
--- assets/js/store.js
+++ assets/js/store.js
@@ -1,16 +1,3 @@
-let data = [
-  {name: 'benjerry', price: 5.95, qtty: 0},
-  {name: 'caffelatte', price: 1.27, qtty: 0},
-  {name: 'calippo', price: 4.10, qtty: 0},
-  {name: 'evax', price: 2.40, qtty: 0},
-  {name: 'pizza', price: 4.95, qtty: 0},
-  {name: 'scottex', price: 4.50, qtty: 0},
-  {name: 'spaghetti', price: 1.25, qtty: 0},
-  {name: 'triangulos', price: 2.35, qtty: 0},
-  {name: 'xibeca', price: 3.75, qtty: 0},
-  {name: 'chipsahoy', price: 2.20, qtty: 0}
-];
-
 let store = {
   data: [ {name: 'benjerry', price: 5.95, qtty: 0},
           {name: 'caffelatte', price: 1.27, qtty: 0},
@@ -23,6 +10,7 @@ let store = {
           {name: 'xibeca', price: 3.75, qtty: 0},
           {name: 'chipsahoy', price: 2.20, qtty: 0},
         ],
+
   // Change data according to action.
   // cb can be passed to act upon the new state.
   change: (obj, cb) => {
@@ -45,7 +33,6 @@ let store = {
     cb(data);
   },
 };
-
 // Example:
 // store.change({action: 'addItem', item: 'pizza'}, (data) => {
 //   console.log(data);
